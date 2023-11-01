b376400dcbeb716c1f7649f8180d356a8f84e48c
MucahitKucuktepe <mehmet20132204@gmail.com>
2023-11-01 23:07:30 +0300

    travers

--- javascript/class-notes/js12_dom-intro/traversingDOM.js
+++ javascript/class-notes/js12_dom-intro/traversingDOM.js
@@ -12,3 +12,26 @@ console.log("**** TRAVERSING DOM ****")
 //? - Yukari yonde (Parent)
 //? - Asagi yonde (children)
 //? - Yatay yonde (Sibling)
+//*yukarı yönde traverse
+const html=document.querySelector("ul li:nth-child(1)")
+console.log(html.parentNode.parentNode.parentNode);
+console.log(html.parentElement.parentElement.parentElement);
+
+//? closest() metodu root yönündeki uygun olan parent'i seçer
+console.log(html.closest("section"));//? en yakın section parent i bulur
+
+console.log(html.closest("main").querySelector("#add-new-item"))//? en yakın main i bul içerisindeki add-new-itwm  adli elemnti bul 
+
+//* Aşagı yönlü Traverse
+//? child, firsElementChild, lastElementChild
+console.log(html);
+console.log(html.firstElementChild);
+console.log(html.firstChild);
+
+const itemList=document.querySelector(".item-list")
+console.log(itemList.firstElementChild);
+console.log(itemList.lastElementChild);
+console.log(itemList.lastElementChild.firstElementChild);
+
+//*-Yatay Yonde Tarvers
+console.log(html.nextElementSibling.innerText);
