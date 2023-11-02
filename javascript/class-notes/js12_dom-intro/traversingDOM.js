6a8208c1440bd99ba3f170dcf21e1a180cdeab02
MucahitKucuktepe <mehmet20132204@gmail.com>
2023-11-02 15:53:53 +0300

    post class end

--- javascript/class-notes/js12_dom-intro/traversingDOM.js
+++ javascript/class-notes/js12_dom-intro/traversingDOM.js
@@ -13,24 +13,30 @@ console.log("**** TRAVERSING DOM ****")
 //? - Yatay yonde (Sibling)
 //*yukarı yönde traverse
 const html=document.querySelector("ul li:nth-child(1)")
-console.log(html.parentNode.parentNode.parentNode);
+console.log(html.innerText);
+
 console.log(html.parentElement.parentElement.parentElement);
 
+//?parentElement en yukarıya gittiğinde null verir.parentNode document e kadar cıkar
+console.log(html.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode);
 //? closest() metodu root yönündeki uygun olan parent'i seçer
 console.log(html.closest("section"));//? en yakın section parent i bulur
+console.log(html.closest("html").querySelector(".add-item").innerText);
 
 console.log(html.closest("main").querySelector("#add-new-item"))//? en yakın main i bul içerisindeki add-new-itwm  adli elemnti bul 
 
 //* Aşagı yönlü Traverse
 //? child, firsElementChild, lastElementChild
 console.log(html);
-console.log(html.firstElementChild);
-console.log(html.firstChild);
+console.log(html.firstElementChild);//?Null
+console.log(html.firstChild);//?HTML
 
 const itemList=document.querySelector(".item-list")
-console.log(itemList.firstElementChild);
-console.log(itemList.lastElementChild);
-console.log(itemList.lastElementChild.firstElementChild);
+console.log(itemList.firstElementChild);//?h3
+console.log(itemList.lastElementChild);//?ul
 
+
+console.log(itemList.lastElementChild.firstElementChild.innerText);
+console.log(itemList.lastElementChild.lastElementChild.innerText);
 //*-Yatay Yonde Tarvers
-console.log(html.nextElementSibling.innerText);
+console.log(html.nextElementSibling.nextElementSibling.previousElementSibling.innerText);
