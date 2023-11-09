7e48dd30e1ebe35ae05b2f81a278ee9924aee757
MucahitKucuktepe <mehmet20132204@gmail.com>
2023-11-09 23:37:29 +0300

    home session end

--- html-css/class-notes/hc16_sass_project/project-inclass/js/app.js
+++ html-css/class-notes/hc16_sass_project/project-inclass/js/app.js
@@ -1,1 +1,9 @@
 //? JS
+
+const hamburgetBtn=document.querySelector(".fa-bars")
+
+hamburgetBtn.addEventListener("click",()=>{
+    const headerLinks=document.querySelector(".header__links")
+    headerLinks.classList.toggle("show")
+
+})
