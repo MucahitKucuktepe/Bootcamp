7fa76c5c6653cbb3be28293ae64ebc4f5151426f
MucahitKucuktepe <mehmet20132204@gmail.com>
2023-11-13 21:01:35 +0300

    added

--- javascript/class-notes/js16_Wallet-App/starter/app.js
+++ javascript/class-notes/js16_Wallet-App/starter/app.js
@@ -7,12 +7,24 @@ const ekleFormu = document.getElementById("ekle-formu");
 //*VARIABLES
 
 let gelirler = 0;
+let harcamaListesi = [];
 
 //*HESAP TABLOSU
 
 const gelirinizTd = document.getElementById("geliriniz");
 const giderinizTd = document.getElementById("gideriniz");
-const kalanTd = document.getElementById("kalan")
+const kalanTd = document.getElementById("kalan");
+
+//*HARCAMA FORMU
+const harcamaFormu = document.getElementById("harcama-formu");
+const harcamaAlanıInput = document.getElementById("harcama-alani");
+const tarihInput = document.getElementById("tarih");
+const miktarInput = document.getElementById("miktar");
+
+//*HARCAMA TABLOSU
+
+const harcamaBody = document.getElementById("harcama-body");
+const temizleBtn = document.getElementById("temizle-btn");
 
 //*EKLE FORMU
 
@@ -21,11 +33,21 @@ ekleFormu.addEventListener("submit", (e) => {
   gelirler = gelirler + Number(gelirInput.value);
   console.log(gelirler);
   localStorage.setItem("gelirler", gelirler);
-  gelirinizTd.innerText=gelirler
+  gelirinizTd.innerText = gelirler;
   ekleFormu.reset();
 });
 //*sayfa yüklendiğinde depodaki gelirin ekrana yazılmasını sağladım
-window.addEventListener("load",()=>{
-    gelirler=Number(localStorage.getItem("gelirler")) || 0
-    gelirinizTd.innerText=gelirler
+window.addEventListener("load", () => {
+  gelirler = Number(localStorage.getItem("gelirler")) || 0;
+  gelirinizTd.innerText = gelirler;
+});
+
+//*
+harcamaFormu.addEventListener("submit",(e)=>{
+    e.preventDefault() //!reload ı engeller
+    const yeniHarcama={
+        
+    }
+
+
 })
