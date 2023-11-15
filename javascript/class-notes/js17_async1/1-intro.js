14cb3865dc7603b131066a847a031a55881b0dea
MucahitKucuktepe <mehmet20132204@gmail.com>
2023-11-15 21:29:39 +0300

    callback Hell

--- javascript/class-notes/js17_async1/1-intro.js
+++ javascript/class-notes/js17_async1/1-intro.js
@@ -32,13 +32,42 @@
 // console.log("bitti");
 // console.timeEnd("basla") //timer ı bitir ve geçen süreyi bana göster
 
-//* Asenkron(SeTimout)
+//* Asenkron(SeTimout) --belli bir süre sonraya zaman kurar
 //* ------------------------------------------------
-console.log("TimeOut kuruldu");
-const timeOut=setTimeout(()=>{
-    console.log("Sure Doldu");
-},1000)
-const timeOut2=setTimeout(()=>{
-    console.log("Sure2 Doldu");
-},0)
-console.log("TimeOut bitti");
+// console.log("TimeOut kuruldu");
+// const timeOut=setTimeout(()=>{
+//     console.log("Sure Doldu");
+// },1000)
+// const timeOut2=setTimeout(()=>{
+//     console.log("Sure2 Doldu");
+// },0)
+// console.log("TimeOut bitti");
+
+//* Asenkron(setInterval, clearInterval) --belli bir süre sonraya zaman kurar
+//* ------------------------------------------------
+// let i =0
+// const zaman =setInterval(()=>{
+//     console.log(++i);
+//     if(i>=5){
+//         clearInterval(zaman)
+//     }
+// },10000)
+
+//! Callback Hell (nested ve birbirine bagli callback'ler)
+//!-----------------------------------------------------
+//* Eger birbirine bagimli asenkron kodlarin yazilmasi gerekirse,nested callback
+//* yapisinin kullanilmasi gerekebilir. Fakat bu iyi bir programlama yaklasimi degildir.
+// !callback hell olarak adlandirilan bu yapinin anlasilmasi ve surdurulebilirligi oldukca zordur.
+
+setTimeout(() => {
+    console.log("john:Hi")
+    setTimeout(() => {
+      console.log("Sarah: Hello")
+      setTimeout(() => {
+        console.log("John: How Are you?")
+        setTimeout(() => {
+          console.log("Sarah:Fine and you?")
+        }, 1000)
+      }, 1000)
+    }, 1000)
+  }, 1000)
