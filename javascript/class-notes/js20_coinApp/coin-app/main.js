2a6a12ef36d300fbcf1545e84b12649dfe1b2aa0
MucahitKucuktepe <mehmet20132204@gmail.com>
2023-11-22 23:08:01 +0300

    delete event

---
+++ javascript/class-notes/js20_coinApp/coin-app/main.js
@@ -0,0 +1,25 @@
+//? Projenin style dosyasını ana js (main.js) kodumuza import ettik
+import "./scss/style.scss";
+import { apiRequest } from "./src/apiRequest";
+
+const form = document.querySelector("header form");
+//?forma saubmit event e tanımladık
+form.addEventListener("submit", (e) => {
+  console.log(e.target);
+  e.preventDefault(); //?tüm submit davranışlarını engelle
+  getCoinData();
+  e.target.reset(); //? formu silme davranısı calıstırır
+});
+
+const getCoinData = async () => {
+  const input = document.querySelector("header form input").value;
+  
+  if (!input.trim()) {
+    alert("Input can not be blank");
+  } else {
+    apiRequest(input);
+  }
+
+  
+};
+//coinranking6a0b3a82d0ba5c11bda27f9bb89f2eb977a7b3a78605f8fe
---
+++ javascript/class-notes/js20_coinApp/coin-app/src/apiRequest.js
@@ -0,0 +1,24 @@
+import { renderCoins } from "./renderData";
+
+export const apiRequest = async (input) => {
+  const API_KEY = "coinranking6a0b3a82d0ba5c11bda27f9bb89f2eb977a7b3a78605f8fe";
+  const options = {
+    headers: {
+      "x-access-token": API_KEY,
+    },
+  };
+
+  const url = `https://api.coinranking.com/v2/coins?search=${input}`;
+  try {
+    const res = await fetch(url, options);
+    const data = await res.json();
+    if (data.data.coins.length === 0) {
+      alert("Coin can not be found");
+    } else {
+    //   console.log(data.data.coins[0]);
+      renderCoins(data.data.coins[0]);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
